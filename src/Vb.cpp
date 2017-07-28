9572cc341df4920bbf9bf4c02f983ce2e53cc584
Tim Simpson <timsimpson4@gmail.com>
2017-07-27 23:05:39 -0500

    Makes random numbers the same each time

--- src/Vb.cpp
+++ src/Vb.cpp
@@ -7,7 +7,15 @@ Vb::Vb(lp3::core::MediaManager & media)
 	rnd_dev(),
     rnd_gen(rnd_dev()),
     rnd_distribution(0.0, 1.0)
-{}
+{
+    // Make the random numbers repeat each time we run the program.
+    rnd_gen.seed(1);
+
+    // If you check this out, it will be "1791095845"
+    // the same as shown in the docs here:
+    // http://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine/seed
+    //   LP3_LOG_INFO("after seed by 1: %d", rnd_gen());
+}
 
 VbFile Vb::OpenForInput(const std::string & filePath) {
 	const std::string full_path = media.path(filePath);
